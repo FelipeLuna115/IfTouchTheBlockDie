3015d98bd323220df8fc31a9aa49fccdf8001041
Felipe Luna <68486389+FelipeLuna115@users.noreply.github.com>
2024-10-01 16:22:01 -0600

    new sections and images added - new functions for making a better user experience

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,50 @@
+const navbarItem = document.querySelector('nav');
+var navbarHeightGeneral = navbarItem.offsetHeight;
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+window.addEventListener('load', function() {
+    const content = document.querySelector('.content');
+    content.style.marginTop = navbarHeightGeneral + 'px';
+});
+window.addEventListener('resize', function() {
+    const content = document.querySelector('.content');
+    content.style.marginTop = navbarHeightGeneral + 'px';
+});
+
 function Goto(urlD) {
-    let url = window.location.href.replace("index.html", "");
-    if (url.includes("#")) {
-        url = url.split("#")[0];
+    const targetElement = urlD === "#" ? null : document.querySelector(urlD);
+
+    if (targetElement) {
+        const elementPosition = targetElement.getBoundingClientRect().top + window.scrollY;
+        const offsetPosition = elementPosition - navbarHeightGeneral;
+        
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    } else {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
     }
-    window.location.replace(url + urlD);
+}
+
+function OpenDocument(doc) {
+    window.open(doc);
+}
+
+function ChangeNavbarStatus() {
+    const navbar = document.getElementById("navbarElements");
+    navbar.hidden = navbar.hidden ? false : true;
+    if (navbar.hidden) {
+        navbarHeightGeneral = navbarItem.offsetHeight;
+    }
+}
+
+function HiddeNavbar() {
+    const navbar = document.getElementById("navbarElements");
+    if (!navbar.hidden) {
+        navbar.hidden = true;
+    }
 }
